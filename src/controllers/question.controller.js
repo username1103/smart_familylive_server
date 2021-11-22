d0a815f11c0a4f33dca2e5cab934089cc809be23
username1103 <username1103@naver.com>
2021-11-22 01:13:18 +0000

    fix: question 만들기 수정

--- src/controllers/question.controller.js
+++ src/controllers/question.controller.js
@@ -5,8 +5,9 @@ const catchAsync = require('../utils/catchAsync');
 const createQuestion = catchAsync(async (req, res) => {
   const { contents } = req.body;
 
-  const questions = await Question.find().sort({ number: 1 });
-  const maxNumber = questions.length !== 0 ? questions[0].number : 0;
+  const question = await Question.findOne().sort({ number: -1 });
+
+  const maxNumber = question === null ? 0 : question.number;
   const result = await Question.create({ number: maxNumber + 1, contents });
 
   res.status(httpStatus.CREATED).send({ result });
