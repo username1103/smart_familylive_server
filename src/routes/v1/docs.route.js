2350db2530eebb319d5ee92f13ec5f36a5c5e71a
username1103 <username1103@naver.com>
2021-11-29 16:32:29 +0000

    feat

--- src/routes/v1/docs.route.js
+++ src/routes/v1/docs.route.js
@@ -10,12 +10,23 @@ const specs = swaggerJsdoc({
   apis: ['src/docs/*.yml', 'src/routes/v1/*.js'],
 });
 
+const swaggerOptions = {
+  swaggerOptions: {
+    operationsSorter: (a, b) => {
+      const methodsOrder = ['post', 'put', 'patch', 'get', 'delete', 'options', 'trace'];
+      let result = methodsOrder.indexOf(a.get('method')) - methodsOrder.indexOf(b.get('method'));
+
+      if (result === 0) {
+        result = a.get('path').localeCompare(b.get('path'));
+      }
+
+      return result;
+    },
+  },
+  explorer: true,
+};
+
 router.use('/', swaggerUi.serve);
-router.get(
-  '/',
-  swaggerUi.setup(specs, {
-    explorer: true,
-  })
-);
+router.get('/', swaggerUi.setup(specs, swaggerOptions));
 
 module.exports = router;
